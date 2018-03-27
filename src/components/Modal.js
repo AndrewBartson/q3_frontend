84a3363f86593e8bb9dbb9702528fbec956213e6
Andrew Bartson <andrewbartson@Andrews-MacBook-Pro.local>
2018-03-27 16:10:36 -0700

    Resolve more merge conflicts

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-
-
-// ---------------------------------------------------------------------
-
->>>>>>> 8e15ee980b14595e0d61b3c1c9f62194836ad387
 import React from 'react';
 import PropTypes from 'prop-types'
 
@@ -13,7 +6,6 @@ export default class Modal extends React.Component {
     super(props);
     this.state = { modalProps: this.props.modalProps };
   }
-
   render() {
     console.log("CURRENT STATE OF MODAL: ")
     console.log(this.props.modalProps);
@@ -23,62 +15,4 @@ export default class Modal extends React.Component {
       </div>
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
-//----------------------------------------------------------------------------
-//
-// `<div class="hoverinfo"><strong>
-//   ${geography.properties.name},
-//   ${data.electoral_votes} electoral votes,
-//   ${data.margin} Margin of Victory
-// </strong></div>`;
-
-//
-// `<div class="hoverinfo"><strong>
-//   ${geography.properties.name}</strong></div>`;
-//
-
-
-
-//
-// responsive: false,
-//       done: function(datamap) {
-//         datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-//           setModalProps({state: true,
-//                          title: geography.properties.name,
-//                          data: []});
-//         });
-//       }
-//
-//
-//
-//
-//
-//   //REDUCER
-//   export default function modalProps(state = {}, action) {
-//   return state;
-// }
-//
-//
-// let modalProps = this.props.modalProps;
-// let setModalProps = this.props.setModalProps;
-//
-//   //APP.JSX
-//   <Modal modalProps={this.state.modalProps}/>
-//
-//   <DataMap
-//             regionData={this.props.regionData}
-//             modalProps={this.state.modalProps}
-//             setModalProps={(newModalProps) => this.setModalProps(newModalProps)}
-//           />
-//
-//
-//   App.propTypes = {
-//   regionData: PropTypes.array.isRequired,
-//   modalProps: PropTypes.object.isRequired,
-//   emptyRegions: PropTypes.array.isRequired,
-//   sortState: PropTypes.object.isRequired
-// };
->>>>>>> 8e15ee980b14595e0d61b3c1c9f62194836ad387
